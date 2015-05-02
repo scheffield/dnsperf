e95ae8ddb51c8e3e95976fdcd74feda905d4fd23
Max Scheffler <mscheffler@dius.com.au>
2015-05-02 15:42:27 +1000

    first simple lookup

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ var colors = require('colors');
 
 module.exports = function (str) {
 
+  var SILENT = { silent: true };
+
   function collectOSInfo() {
     if (os.platform().toLowerCase() === 'darwin') {
-      var version = shell.exec('sw_vers -productVersion', { silent: true }).output.trim();
+      var version = shell.exec('sw_vers -productVersion', SILENT).output.trim();
       var versionA = version.split('.');
 
       return {
-        osx: false,
+        osx: true,
         version: version,
         major: versionA[0],
         minor: versionA[1],
@@ -21,7 +23,19 @@ module.exports = function (str) {
     }
 
     return {osx: false};
-  };
+  }
+
+  function dig(target, dns) {
+    return shell.exec(['dig @', dns, ' ', target, ' | grep "Query time:"'].join(''), SILENT);
+  }
+
+  // @returns ms to perform the lookup
+  // TODO (scheffield): explain mesurement: http://blog.easydns.org/2011/05/02/dns-speeds-debunked/
+  function mesureLookupTime(target, dns) {
+    var output = dig(target, dns).output.trim();
+
+    return parseInt(/;; Query time: ([0-9]+) msec/.exec(output)[1], 10);
+  }
 
   var osInfo = collectOSInfo();
   console.log(osInfo);
@@ -30,5 +44,13 @@ module.exports = function (str) {
     console.log('At the moment only OSX is supported!'.red);
     return;
   }
+
+  //TODO (scheffield): check for cli apps
+
+  for (var i = 0; i < 10; i++) {
+    //TODO (scheffield): consider return code
+    console.log(mesureLookupTime('heise.de', '8.8.8.8'));
+  }
+
 
 };
